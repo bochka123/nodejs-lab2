f8c9a2b8b153d8248326b487df79a24e5147244b
bochka123 <sasha2003tkach001100@gmail.com>
2023-03-08 15:07:54 +0200

    fixing modules

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import http, {IncomingMessage, ServerResponse} from 'http';
+import request from 'request';
+import cheerio from 'cheerio';
+
+const url = "https://www.wunderground.com/cgi-bin/findweather/getForecast?&query=";
 
 const PORT = 3000;
 
 const server = http.createServer((req: IncomingMessage, res: ServerResponse) : void => {
-    res.end();
+    request(url, function (error, response, body) {
+        if (!error) {
+            const $ = cheerio.load(body),
+                temperature = $("[data-variable='temperature'] .wx-value").html();
+            res.end(body);
+            console.log("It’s " + temperature + " degrees Fahrenheit.");
+        } else {
+            console.log("We’ve encountered an error: " + error);
+        }
+    });
 });
 
 server.listen(PORT);
