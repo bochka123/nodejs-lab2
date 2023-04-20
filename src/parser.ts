5f3e26332dc34d2dee4608d48f0dcaff4639fb8e
bochka123 <sasha2003tkach001100@gmail.com>
2023-04-20 12:16:21 +0300

    making url readonly

--- src/parser.ts
+++ src/parser.ts
@@ -5,7 +5,7 @@ import { IPost } from './IPost';
 
 export class Parser {
 
-    url = 'https://uareview.com/';
+    readonly url = 'https://uareview.com/';
 
     parseSite(): void {
         request(this.url, function (error, response, body) {
